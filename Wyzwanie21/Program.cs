f29ff0b09fc5764138fdb1e2c1115374462899fa
ALBER7K <albertkulawik@gmail.com>
2025-02-06 19:42:02 +0100

    Dzien IV

--- Wyzwanie21/Program.cs
+++ Wyzwanie21/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, people!");
+﻿string gender = "K";
+string name = "Ewa";
+int age = 19;
+
+if (gender == "K" && age < 30)
+    Console.WriteLine("Kobieta ponizej 30 lat");
+else if (name == "Ewa" && age == 30)
+    Console.WriteLine("Ewa, lat 30");
+else if (gender != "K" && age < 18)
+    Console.WriteLine("Niepelnoletni mezczyzna");
+
