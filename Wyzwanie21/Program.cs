a5b78da355d71ec36940b9a522d2a567664465c3
ALBER7K <albertkulawik@gmail.com>
2025-02-08 09:18:11 +0100

    Stringi i foreach

--- Wyzwanie21/Program.cs
+++ Wyzwanie21/Program.cs
@@ -1,11 +1,13 @@
-﻿string gender = "K";
-string name = "Ewa";
-int age = 19;
+﻿List<string> daysOfWeek = new List<string>();
+daysOfWeek.Add("pon");
+daysOfWeek.Add("wt");
+daysOfWeek.Add("sr");
+daysOfWeek.Add("czw");
+daysOfWeek.Add("pt");
+daysOfWeek.Add("sb");
+daysOfWeek.Add("ndz");
 
-if (gender == "K" && age < 30)
-    Console.WriteLine("Kobieta ponizej 30 lat");
-else if (name == "Ewa" && age == 30)
-    Console.WriteLine("Ewa, lat 30");
-else if (gender != "K" && age < 18)
-    Console.WriteLine("Niepelnoletni mezczyzna");
-
+foreach (string day in daysOfWeek)
+{
+    Console.WriteLine(day);
+}
