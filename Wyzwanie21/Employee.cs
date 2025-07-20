ba379dddc05e63396acc78f7789ac0c260009dc4
ALBER7K <albertkulawik@gmail.com>
2025-07-20 18:08:14 +0200

    Dzien XII przygotowanie

--- Wyzwanie21/Employee.cs
+++ Wyzwanie21/Employee.cs
@@ -40,22 +40,6 @@ namespace Wyzwanie21
                 Console.WriteLine($"String, to nie float - zmien swoja ocene z '{grade}' na jakas cyfre lub wartosc liczbowa!");
             }
         }
-        public void AddGrade(decimal grade)
-        {
-            float ValueInFloat = (float)grade;
-            this.AddGrade(ValueInFloat);
-        }
-        public void AddGrade(double grade)
-        {
-            float ValueInFloat = (float)grade;
-            this.AddGrade(ValueInFloat);
-        }
-        public void AddGrade(int grade)
-        {
-            float ValueInFloat = (float)grade;
-            this.AddGrade(ValueInFloat);
-
-        }
         public Statistics GetStatisticks()
         {
             var statistics = new Statistics();
@@ -65,9 +49,6 @@ namespace Wyzwanie21
 
             List<string> formattedGrades = new List<string>();
             foreach (var grade in this.grades)
-            //break
-            //continue
-            //goto
 
             {
                 if (grade >= 0 && grade <= 100)
@@ -93,102 +74,5 @@ namespace Wyzwanie21
 
             return statistics;
         }
-        public Statistics GetStatisticksWithForEach()
-        {
-            var statisticks = new Statistics();
-            statisticks.Average = 0;
-            if (this.grades.Count == 0)
-            {
-                Console.WriteLine("Brak ocen do obliczen.");
-                statisticks.Max = float.MinValue;
-                statisticks.Min = float.MaxValue;
-                return statisticks;
-            }
-            float? firstValidGrade = null;
-            foreach (var X in this.grades)
-            {
-                if (X >= 0 && X <= 5)
-                {
-                    statisticks.Max = X;
-                    statisticks.Min = X;
-                    firstValidGrade = X;
-                    break;
-                }
-            }
-            if (!firstValidGrade.HasValue)
-            { Console.WriteLine("Brak ocen z zakresu 0-5."); return statisticks; }
-
-            int validGradesCount = 0;
-            Console.WriteLine("Wyniki dla petli foreach z zakresu 0-5:");
-            foreach (var grade in this.grades)
-            {
-                if (grade >= 0 && grade <= 5)
-                {
-                    statisticks.Max = Math.Max(statisticks.Max, grade);
-                    statisticks.Min = Math.Min(statisticks.Min, grade);
-                    statisticks.Average += grade;
-                    validGradesCount++;
-                    Console.WriteLine($"{grade}");
-                }
-
-            }
-            return statisticks;
-        }
-        public Statistics GetStatisticksWithFor()
-        {
-            var statisticks = new Statistics();
-
-            if (this.grades.Count == 0)
-            {
-                Console.WriteLine("Brak ocen do obliczen.");
-                statisticks.Average = 0;
-                statisticks.Max = float.MinValue;
-                statisticks.Min = float.MaxValue;
-                return statisticks;
-            }
-
-            statisticks.Average = 0;
-            statisticks.Max = float.MinValue;
-            statisticks.Min = float.MaxValue;
-
-            for (int i = 0; i < this.grades.Count; i++)
-            {
-                float grade = this.grades[i];
-                statisticks.Max = Math.Max(statisticks.Max, grade);
-                statisticks.Min = Math.Min(statisticks.Min, grade);
-                statisticks.Average += grade;
-            }
-
-            statisticks.Average /= this.grades.Count;
-            Console.WriteLine($"Wyniki dla petli for:");
-            Console.WriteLine($"Srednia: {statisticks.Average:N2}");
-            Console.WriteLine($"Maksymalna: {statisticks.Max}");
-            Console.WriteLine($"Minimalna: {statisticks.Min}");
-
-            return statisticks;
-        }
-        public Statistics GetStatisticksWithWhile()
-        {
-            var statisticks = new Statistics();
-            statisticks.Average = 0;
-            statisticks.Max = float.MinValue;
-            statisticks.Min = float.MaxValue;
-            int i = 0;
-            while (i < this.grades.Count)
-            {
-                if (this.grades[i] <= 0 || this.grades[i] >= 100)
-                {
-                    statisticks.Max = Math.Max(statisticks.Max, this.grades[i]);
-                    statisticks.Min = Math.Min(statisticks.Min, this.grades[i]);
-                    statisticks.Average += this.grades[i];
-                }
-                Console.WriteLine($"Petla WithWile czyli wszystko od 0 do 100: {this.grades[i]}");
-                i++;
-            }
-            statisticks.Average /= this.grades.Count;
-            return statisticks;
-        }
     }
 }
-
-
--- Wyzwanie21/Program.cs
+++ Wyzwanie21/Program.cs
@@ -17,17 +17,5 @@ employee.AddGrade(5.55555555555f);
 employee.AddGrade(5.55555555555f);
 employee.AddGrade(4);
 employee.AddGrade(101);
+
 var statisticks = employee.GetStatisticks();
-var statisticks1 = employee.GetStatisticksWithForEach();
-var statisticks2 = employee.GetStatisticksWithFor();
-var statisticks3 = employee.GetStatisticksWithWhile();
-
-//Console.WriteLine($"Average: {statisticks.Average:N2}");
-//Console.WriteLine($"Max: {statisticks.Max:N2}");
-//Console.WriteLine($"Min: {statisticks.Min:N2}");
-
-//SetSth(out statisticks);
-//void SetSth(out Statisticks statisticks)
-//{
-//    statisticks = new Statisticks();
-//}
